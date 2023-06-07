3a69da0c386547ae3d663291cc06f342c5b59536
Ilya <anamkaj@gmail.com>
2023-06-07 11:29:01 +0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CategoryModule } from './modules/category/category.module'
 import { ReviewsModule } from './modules/reviews/reviews.module'
 import { ProductModule } from './modules/product/product.module'
 import { CountsModule } from './modules/count/counts.module'
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule, FormModule, ReviewsModule, CountsModule, SearchModule],
+  imports: [ProductModule, CategoryModule, FormModule, ReviewsModule, CountsModule, SearchModule, PostModule],
   controllers: [],
 })
 export class AppModule {}
