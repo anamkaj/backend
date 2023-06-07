2d1d303079c5d261d4a0e923e3c3cfdf485d4b7e
Ilya <anamkaj@gmail.com>
2023-06-07 12:30:03 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module'
 import path, { join } from 'path'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
+import { config } from 'dotenv'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors()
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
+  config()
 
   app.useStaticAssets(join(__dirname, '..', 'public', 'img'), {
     prefix: '/img',
