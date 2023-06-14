51f0593c0ac392485c9c3814d67d9a4457961d65
Ilya <anamkaj@gmail.com>
2023-06-14 10:27:51 +0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { config } from 'dotenv'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    httpsOptions: {
-      key: '/etc/ssl/tmk-v.ru.crt',
-      ca: 'ca',
-      cert: 'cert',
-    },
-  })
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors()
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
