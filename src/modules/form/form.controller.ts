232425bcdd3f11d8fd23321f89fc2879e4aef3a9
Ilya <anamkaj@gmail.com>
2023-06-18 13:53:33 +0300

    updste

--- src/modules/form/form.controller.ts
+++ src/modules/form/form.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Post, Body, HttpCode, UsePipes, ValidationPipe } from 
 import { FormService } from './form.service'
 import { DtoDataForm } from './dto/create-form.dto'
 
-
 @Controller('form')
 export class FormController {
   constructor(private readonly formService: FormService) {}
@@ -10,14 +9,19 @@ export class FormController {
   @Post('/order')
   @HttpCode(200)
   async fastOrder(@Body() FormOrder: DtoDataForm) {
-   
     return this.formService.fastOrder(FormOrder)
+  }
+
+  @Post('/order/oneProduct')
+  @HttpCode(200)
+  async oneProduct(@Body() FormOrder: DtoDataForm) {
+    return this.formService.oneProduct(FormOrder)
   }
 
   @Post('/order/cart')
   @HttpCode(200)
   async orderFormCartPage(@Body() FormOrder: DtoDataForm) {
-  
+    console.log(FormOrder)
     return this.formService.orderFormCartPage(FormOrder)
   }
 }
--- src/modules/form/form.service.ts
+++ src/modules/form/form.service.ts
@@ -7,26 +7,29 @@ export class FormService {
   constructor(private readonly mailerService: MailerService) {}
   async fastOrder(FormOrder: DtoDataForm) {
     if (!FormOrder) return 'В форме ошибка'
-    console.log(FormOrder)
-    return this.example(FormOrder)
+    return this.sendFormFeedback(FormOrder)
   }
 
   async orderFormCartPage(FormOrder: DtoDataForm) {
     if (!FormOrder) return 'В форме ошибка'
-    console.log(FormOrder)
-    return 'форма со страницы корзины'
+    return this.sendCartPage(FormOrder)
   }
 
-  public example(FormOrder: DtoDataForm): void {
+  async oneProduct(FormOrder: DtoDataForm) {
+    if (!FormOrder) return 'В форме ошибка'
+    console.log(FormOrder)
+    return this.sendCartPage(FormOrder)
+  }
+
+  public sendCartPage(FormOrder: DtoDataForm): void {
     const { data } = FormOrder
-    const cart = data.data.map((x, index) => [
+    const cart = data.data.map(x => [
       `${x.title}`,
       `Количество: ${x.count} шт. `,
       `Размер скидки: ${x.sale}%. `,
       `Стоимость за одну штуку: ${x.price} (без скидка)`,
       `<br>`,
     ])
-    console.log(cart)
 
     this.mailerService
       .sendMail({
@@ -39,6 +42,56 @@ export class FormService {
         <h2 style="color: black; font-size: 14px">Телефон: ${data.phone}</h2>
         <h2 style="color: black; font-size: 14px">Почта: ${data.email}</h2>
         <h2 style="color: black; font-size: 14px; font-family: monospace; font-weight: lighter;">Корзина: ${'<br>'} ${cart}</h2>
+        </div>`, // HTML body content
+      })
+      .then(success => {
+        console.log(success)
+        return 'Форма отправленна'
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  public sendFormFeedback(FormOrder: DtoDataForm): void {
+    const { data } = FormOrder
+
+    this.mailerService
+      .sendMail({
+        to: 'tmk-v.ru@yandex.ru', // list of receivers
+        from: 'tmk-v.ru@yandex.ru', // sender address
+        subject: 'Корзина Покупки ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: `<div> 
+        <h1 style="color: black; font-size: 14px"> Имя: ${data.name}</h1>
+        <h2 style="color: black; font-size: 14px">Телефон: ${data.phone}</h2>
+        <h2 style="color: black; font-size: 14px">Почта: ${data.email}</h2>
+        
+        </div>`, // HTML body content
+      })
+      .then(success => {
+        console.log(success)
+        return 'Форма отправленна'
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  public sendFormOneProduct(FormOrder: DtoDataForm): void {
+    const { data } = FormOrder
+
+    this.mailerService
+      .sendMail({
+        to: 'tmk-v.ru@yandex.ru', // list of receivers
+        from: 'tmk-v.ru@yandex.ru', // sender address
+        subject: 'Корзина Покупки ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: `<div> 
+        <h1 style="color: black; font-size: 14px"> Имя: ${data.name}</h1>
+        <h2 style="color: black; font-size: 14px">Телефон: ${data.phone}</h2>
+        <h2 style="color: black; font-size: 14px">Почта: ${data.email}</h2>
+        
         </div>`, // HTML body content
       })
       .then(success => {
